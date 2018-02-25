d33ef7c552d3aacf86d0c07bdade08b840c4e580
arodg <anitarodgerson@gmail.com>
2018-02-25 16:43:52 -0600

    Add 4 friends

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+var friendsArray = [
+  {
+    name: "Wonder Woman",
+    photo: "images/WonderWoman.jpeg",
+    scores:[5, 4, 2, 3, 2, 5, 1, 3, 1, 1]
+  },
+  {
+  	name: "Batman",
+  	photo: "images/Batman.jpeg",
+  	scores: [5, 2, 2, 1, 5, 2, 3, 5, 3, 5]
+  },
+  {
+    name: "Superman",
+    photo: "images/Superman.png",
+    scores: [5, 2, 2, 1, 3, 4, 3, 5, 3, 5]
+  }
+  {
+    name: "Incredible Hulk",
+    photo: "images/IncredibleHulk.jpeg",
+    scores: [5, 4, 1, 2, 5, 1, 5, 4, 3, 1]
+  }
+];
+
+module.exports = friendsArray;
+
