3f42024793135458e718c6e68cbae9298a091feb
arodg <anitarodgerson@gmail.com>
2018-02-25 20:22:19 -0600

    Finish dummy data

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,11 +13,31 @@ var friendsArray = [
     name: "Superman",
     photo: "images/Superman.png",
     scores: [5, 2, 2, 1, 3, 4, 3, 5, 3, 5]
-  }
+  },
   {
     name: "Incredible Hulk",
     photo: "images/IncredibleHulk.jpeg",
     scores: [5, 4, 1, 2, 5, 1, 5, 4, 3, 1]
+  },
+  {
+    name: "Black Widow",
+    photo: "BlackWidow.jpeg",
+    scores: [5, 2, 1, 4, 5, 4, 3, 5, 2, 1]
+  },
+  {
+    name: "Aquaman",
+    photo: "images/Aquaman.jpeg",
+    scores: [5, 2, 1, 4, 5, 4, 3, 5, 2, 1]
+  },
+  {
+    name: "Spider-Man",
+    photo: "images/SpiderMan.png",
+    scores: [5, 1, 2, 4, 5, 2, 3, 4, 5, 1]
+  },
+  {
+    name: "Black Panther",
+    photo: "images/BlackPanther.jpeg", 
+    scores: [5, 1, 2, 2, 4, 3, 3, 4, 5, 1]
   }
 ];
 
